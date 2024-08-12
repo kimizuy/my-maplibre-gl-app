9f9b682ea9284e0d300dbcc68e6d42563936e6a7
kimizuy <kimizuyamasaki@gmail.com>
2024-08-12 16:17:23 -0700

    manage map instance with useState

--- src/map-component.tsx
+++ src/map-component.tsx
@@ -1,14 +1,15 @@
-import { useEffect, useRef } from "react";
-import maplibregl from "maplibre-gl";
+import { useEffect, useRef, useState } from "react";
+import maplibregl, { Map } from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 
 export const MapComponent = () => {
   const mapContainer = useRef(null);
+  const [map, setMap] = useState<Map | null>(null);
 
-  useEffect(() => {
+  useEffect(function initializeMap() {
     if (!mapContainer.current) return;
 
-    const map = new maplibregl.Map({
+    const mapInstance = new maplibregl.Map({
       container: mapContainer.current,
       hash: true,
       style:
@@ -20,11 +21,22 @@ export const MapComponent = () => {
       localIdeographFontFamily: false,
     });
 
-    map.addControl(new maplibregl.NavigationControl(), "bottom-right");
-    map.addControl(new maplibregl.ScaleControl());
+    mapInstance.addControl(new maplibregl.NavigationControl(), "bottom-right");
+    mapInstance.addControl(new maplibregl.ScaleControl());
 
-    return () => map.remove();
+    setMap(mapInstance);
+
+    return () => mapInstance.remove();
   }, []);
+
+  useEffect(
+    function updateMap() {
+      if (!map) return;
+
+      new maplibregl.Marker().setLngLat([139.767144, 35.680621]).addTo(map);
+    },
+    [map]
+  );
 
   return (
     <div
